e3f31fadb65f17ac496ed16d3fe08ee46f20635f
Lukas P <82966413+Lukasjar@users.noreply.github.com>
2023-02-22 14:38:15 +0100

    Random Passwords

---
+++ randomgenerator.py
@@ -0,0 +1,12 @@
+# Copyright Lukas Pellny | 2023
+import random
+
+Lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+Capitalized_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '012345678'
+symbols = '1§$%&=?#'
+combination = Lowercase_letters + Capitalized_letters + numbers + symbols
+length = 8
+password = ''.join(random.sample(combination, length))
+print(password) 
+

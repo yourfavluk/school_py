d026b35e6a2e28d8092a030f4c658be1ce657c7e
Lukas P <82966413+Lukasjar@users.noreply.github.com>
2023-02-22 14:41:41 +0100

    Update randomgenerator.py

--- randomgenerator.py
+++ randomgenerator.py
@@ -5,8 +5,8 @@ Lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 Capitalized_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers = '012345678'
 symbols = '1§$%&=?#'
-combination = Lowercase_letters + Capitalized_letters + numbers + symbols
+Execution = Lowercase_letters + Capitalized_letters + numbers + symbols
 length = 9
-password = ''.join(random.sample(combination, length))
+password = ''.join(random.sample(Execution, length))
 print(password) 
 

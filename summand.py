9c8066e25d7afdc350df6be969c3ad2c8e30be34
Lukas P <82966413+Lukasjar@users.noreply.github.com>
2023-03-19 11:34:31 +0100

    Add files via upload

---
+++ summand.py
@@ -0,0 +1,9 @@
+# Rechnet summand1 + summand2 den man in der Kommandozeile eingibt.
+summand1=int(input())
+summand2=int(input())
+
+
+print(summand1+summand2)
+
+# Hier wird durch "*" Mal gerechnet.
+print(summand1*summand2)
---
+++ zweierreihe.py
@@ -0,0 +1,7 @@
+# Zweierrheie von 2-2ooo
+a = 2
+while(a<1000):
+    print(a)
+    a=a+2
+    
+    
